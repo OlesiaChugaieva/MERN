bfdeb6c8d4baa681005c0fc6594a39adcf35b09b
OLESIA CHUGAIEVA <ALESYAPRITCHARD@GMAIL.COM>
2022-11-09 21:11:02 -0500

    typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,14 +1,33 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+type Query {
+    me: User
+  }
+
   type User {
+    _id: ID!
     username: String!
     email: String!
-    password: String!
+   bookCount: Int
+   savedBooks: [Book]
   }
-  type Query {
-    getSingleUser: User
-  }
+
+  type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+  saveBook(input: BookInput!): User
+  removeBook(bookId: ID!): User
+}
+
+type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
 `;
 
 module.exports = typeDefs;
