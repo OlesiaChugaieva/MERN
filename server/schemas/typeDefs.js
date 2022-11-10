5b187e4ce14c263a0fbb0dc17752c39865943126
OLESIA CHUGAIEVA <ALESYAPRITCHARD@GMAIL.COM>
2022-11-09 19:26:36 -0500

    schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,14 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    username: String!
+    email: String!
+    password: String!
+  }
+  type Query {
+    getSingleUser: User
+  }
+`;
+
+module.exports = typeDefs;
